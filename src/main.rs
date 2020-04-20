c32ff2e264982b626828ee04992f64e2af58b559
Steven Waldron <steven.waldron@pm.me>
2020-04-20 10:34:29 -0500

    Created outline of the program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::env;
+
+fn eratosthenes(sieve: Vec<i32>) -> Vec<i32> {
+    sieve
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let sieve: Option<Vec<i32>>;
+
+    sieve = if let Some(i) = args.get(1) {
+        if let Ok(i) = i.parse::<i32>() {
+            Some((1..i).collect())
+        } else {
+            None
+        }
+    } else {
+        None
+    };
+
+    let answer = match sieve {
+        Some(sieve) => eratosthenes(sieve),
+        None => eratosthenes((1..30).collect()),
+    };
+
+    println!("{:?}", answer);
 }
